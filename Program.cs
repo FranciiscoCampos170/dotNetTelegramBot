3607a3098eff6e7c441c255424c88c8216ae24da
Francisco Campos <franciiscocampos170@gmail.com>
2021-12-12 20:56:30 +0100

    configure bot

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Telegram.Bot;
+
+var botClient = new TelegramBotClient("5068974834:AAHWdjg7uth1MU0c5JYsxBQmErpAvJnOR4U");
+
+var me = await botClient.GetMeAsync();
+Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
